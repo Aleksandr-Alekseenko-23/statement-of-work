3c74ff46a79dc9497c0155926935520e23398e29
Aleksandr-Alekseenko-23 <alekseenko48@bigmir.net>
2023-04-18 19:33:26 +0300

    fix5

---
+++ dist/assets/HomePage-bf1d4968.js
@@ -0,0 +1,19 @@
+import{s as e,j as t,F as o,a as n}from"./index-75dfd4f6.js";const s=e.span`
+  position: absolute;
+  top: 150px;
+  left: 385px;
+  font-family: "Montserrat";
+  font-style: normal;
+  font-weight: 600;
+  font-size: 26px;
+  line-height: 22px;
+  text-transform: uppercase;
+  color: #4611ba;
+`,r=e.img`
+  text-align: center;
+`,a=e.div`
+  background-color: #1ca2eb;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`,c="/assets/background-dbb96f5e.jpg",l=()=>t(o,{children:n(a,{children:[t(s,{children:" Your homemade twitter"}),t(r,{src:c})]})});export{l as default};
